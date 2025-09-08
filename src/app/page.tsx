cadecefe4e40bb60fa4129a4cff9ed7520594bee
blackboxai-deploy <deploy@blackboxai.dev>
2025-09-08 15:03:42 +0530

    Add src/app/page.tsx

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import PubgGame from "@/components/PubgGame";
+
+export default function Home() {
+  return (
+    <main className="w-screen h-screen overflow-hidden bg-black">
+      <PubgGame />
+    </main>
+  );
+}
